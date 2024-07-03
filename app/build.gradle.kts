6ac0e2aa6b49e9b9a060b62d62d4c3998ab0a874
Matias Alzú <102830508+Sokamn@users.noreply.github.com>
2024-07-03 15:25:20 -0300

    Design finished
    Inputs & Button style finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.extraProperties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -59,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/sokamn/logininstagram/LoginScreen.kt
+++ app/src/main/java/com/sokamn/logininstagram/LoginScreen.kt
@@ -17,9 +17,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -33,8 +38,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,9 +54,11 @@ import androidx.compose.ui.unit.sp
 )
 @Composable
 fun LoginScreen() {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .padding(8.dp)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
         Header(Modifier.align(Alignment.TopEnd))
         Body(Modifier.align(Alignment.Center))
         Footer(Modifier.align(Alignment.BottomCenter))
@@ -76,7 +86,7 @@ fun SignUp() {
         Text(color = Color(0xFFB5B5B5), fontSize = 12.sp, text = "Don't have account?")
         Spacer(modifier = Modifier.size(2.dp))
         Text(
-            color = Color(0xFF4EA8E9),
+            color = Color(0xFF3997f1),
             fontSize = 12.sp,
             fontWeight = FontWeight.Bold,
             text = "SignUp."
@@ -123,7 +133,7 @@ fun SocialLogin() {
         )
         Text(
             modifier = Modifier.padding(horizontal = 8.dp),
-            color = Color(0xFF4EA8E9),
+            color = Color(0xFF3997f1),
             text = "Log In with Facebook",
             fontSize = 14.sp,
             fontWeight = FontWeight.Bold
@@ -158,7 +168,18 @@ fun LoginDivider() {
 
 @Composable
 fun LoginButton(loginEnable: Boolean) {
-    Button(onClick = {}, enabled = loginEnable, modifier = Modifier.fillMaxWidth()) {
+    Button(
+        onClick = {},
+        enabled = loginEnable,
+        shape = MaterialTheme.shapes.small,
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF3997f1),
+            disabledContainerColor = Color(0xFF78C8F9),
+            contentColor = Color.White,
+            disabledContentColor = Color.White
+        )
+    ) {
         Text(text = "Log In")
     }
 }
@@ -170,27 +191,46 @@ fun ForgotPassword(modifier: Modifier) {
         text = "Forgot password?",
         fontSize = 12.sp,
         fontWeight = FontWeight.Bold,
-        color = Color(0xFF4EA8E9)
+        color = Color(0xFF3997f1)
     )
 }
 
 @Composable
 fun PasswordField(password: String, onTextChanged: (String) -> Unit) {
+    var passVisibility by remember { mutableStateOf(false) }
     TextField(
         modifier = Modifier.fillMaxWidth(),
         value = password,
-        placeholder = { Text(text = "Password")},
+        placeholder = { Text(text = "Password") },
         maxLines = 1,
         singleLine = true,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
         colors = TextFieldDefaults.colors(
             unfocusedIndicatorColor = Color.Transparent,
             unfocusedContainerColor = Color(0xFFFAFAFA),
-            unfocusedTextColor = Color(0xFFA9A9A9),
+            unfocusedTextColor = Color(0xFFB2B2B2),
+            unfocusedPlaceholderColor = Color(0xFFA9A9A9),
+            focusedPlaceholderColor = Color(0xFFA9A9A9),
             focusedIndicatorColor = Color.Transparent,
             focusedTextColor = Color(0xFFB2B2B2),
             focusedContainerColor = Color(0xFFFAFAFA)
-            ),
+        ),
+        trailingIcon = {
+            val passVisibilityIcon = if (passVisibility) {
+                Icons.Filled.VisibilityOff
+            } else {
+                Icons.Filled.Visibility
+            }
+
+            IconButton(onClick = { passVisibility = !passVisibility }) {
+                Icon(imageVector = passVisibilityIcon, contentDescription = "Password Visibility")
+            }
+        },
+        visualTransformation = if (passVisibility) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
         onValueChange = { onTextChanged(it) })
 }
 
@@ -199,7 +239,7 @@ fun EmailField(email: String, onTextChanged: (String) -> Unit) {
     TextField(
         modifier = Modifier.fillMaxWidth(),
         value = email,
-        placeholder = { Text(text = "Phone, number, email adress or username")},
+        placeholder = { Text(text = "Phone, number, email adress or username") },
         maxLines = 1,
         singleLine = true,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
@@ -207,17 +247,12 @@ fun EmailField(email: String, onTextChanged: (String) -> Unit) {
             unfocusedIndicatorColor = Color.Transparent,
             unfocusedContainerColor = Color(0xFFFAFAFA),
             unfocusedTextColor = Color(0xFFA9A9A9),
+            unfocusedPlaceholderColor = Color(0xFFB2B2B2),
+            focusedPlaceholderColor = Color(0xFFB2B2B2),
+            focusedTextColor = Color(0xFFA9A9A9),
             focusedIndicatorColor = Color.Transparent,
-            focusedTextColor = Color(0xFFB2B2B2),
             focusedContainerColor = Color(0xFFFAFAFA)
         ),
-        trailingIcon = {
-            val image = if(){
-                Icons.Default.
-            }else{
-
-            }
-        },
         onValueChange = { onTextChanged(it) })
 }
 
